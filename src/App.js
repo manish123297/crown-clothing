12459aead4236dbfa993a2b8c491894039ed4074
manish <chaudharymanish684@gmail.com>
2022-12-07 12:39:02 +0530

    Intro and setup(section:4):added directory and category item component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
-import "./App.css";
-
+import Directory from "./directory/directory .component";
 function App() {
-  return (
-    <div className="App">
-      <h1>Starting of the project</h1>
-    </div>
-  );
+  const categories = [
+    {
+      id: 1,
+      title: "Hats",
+      imageUrl:
+        "https://nova-live.imgix.net//image-279c4697-8243-4951-96a5-ca3dbc80656b.png?q=60&fm=webp&fit=crop&fp-x=0.5&crop=focalpoint&fp-y=0.5&rot=0&fp-z=1&w=700&h=466",
+    },
+    {
+      id: 2,
+      title: "Jackets",
+      imageUrl:
+        "https://www.thepostcity.com/wp-content/uploads/2021/09/leather-jackets-collection.jpg",
+    },
+    {
+      id: 3,
+      title: "Sneakers",
+      imageUrl:
+        "https://static01.nyt.com/images/2021/05/27/fashion/24SNEAKERCOLORS-sacai-SUB/24SNEAKERCOLORS-sacai-SUB-superJumbo.jpg",
+    },
+    {
+      id: 4,
+      title: "Womens",
+      imageUrl:
+        "https://media.glamour.com/photos/61ae3506d56654ca38186abd/1:1/w_2000,h_2000,c_limit/best%20winter%20dresses.jpg",
+    },
+    {
+      id: 5,
+      title: "Mens",
+      imageUrl:
+        "https://staticg.sportskeeda.com/editor/2022/06/7212b-16557249225203-1920.jpg",
+    },
+  ];
+  return <Directory categories={categories}></Directory>;
 }
 
 export default App;
---
+++ src/category-item/category-item.component.jsx
@@ -0,0 +1,20 @@
+import "./category-item.styles.scss"
+
+const CategoryItem=( {category})=>{
+    const {id,title,imageUrl}=category;
+  
+    return   <div  key={id} className="category-container">
+    <div
+      className="background-image"
+      style={{ backgroundImage: `url(${imageUrl})` }}
+    />
+    {/* here we have passed the style as the object */}
+    <div className="category-body-container">
+      <h2>{title}</h2>
+      <p>Shop now</p>
+    </div>
+  </div>
+
+}
+
+export default CategoryItem;
